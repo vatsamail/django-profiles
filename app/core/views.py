7befb21257fe64168eaac91a908be1d4b01bf0f2
Vatsa Prahallada <svatsa@qti.qualcomm.com>
2018-06-15 11:00:44 -0700

    friday before wrap up

--- app/core/views.py
+++ app/core/views.py
@@ -1,5 +1,21 @@
 from django.views.generic import TemplateView
+from django.shortcuts import render, redirect
+
+from core.forms import HomeForm
+
 
 class HomeView(TemplateView):
     template_name = 'core/home.html'
-    
+
+    def get(self, request):
+        form = HomeForm()
+        args = {'form': form}
+        return render(request, self.template_name, args)
+
+    def post(self, request):
+        form = HomeForm(request.POST)
+        if form.is_valid():
+            text = form.cleaned_data['post']
+            return redirect('ui:profile')
+            args = {'form': form, 'text': text}
+            return render(request, self.template_name, args)
